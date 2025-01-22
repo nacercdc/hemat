c3bc1ec98c4254ec7fa3971ad0d4d1ffba5971cf
Kabtamu Degifie <kabtamu@etmsoftwareplc.com>
2025-01-22 14:46:13 +0300

    feat: input

---
+++ apps/expo/src/utils/object.ts
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/no-empty-object-type */
+export function omit<T extends {}, K extends keyof T>(obj: T, ...keys: K[]) {
+  return Object.fromEntries(
+    Object.entries(obj).filter(([key]) => !keys.includes(key as K))
+  ) as Omit<T, K>;
+}
+
+export function pick<T extends {}, K extends keyof T>(obj: T, ...keys: K[]) {
+  return Object.fromEntries(
+    keys.filter((key) => key in obj).map((key) => [key, obj[key]])
+  ) as Pick<T, K>;
+}
