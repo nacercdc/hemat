3e0a8bdfa2dc06fd585cb5420946405ef938a554
Kabtamu Degifie <kabtamu@etmsoftwareplc.com>
2025-01-23 15:30:54 +0300

    feat: create patch to ui kitten v5.3.1 to solve warning issue

--- apps/expo/src/app/(stack)/index.tsx
+++ apps/expo/src/app/(stack)/index.tsx
@@ -1,20 +1,28 @@
-import { View, Text } from "react-native";
+import { View } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import useGetLanguages from "~/libs/rn-firestore/hooks/example.hooks";
+import { Datepicker, Layout } from "@ui-kitten/components";
+import Text from "~/components/core/presentations/text/Text";
 
 export default function Index() {
   const { data, ...state } = useGetLanguages();
 
+  const [date, setDate] = React.useState(new Date());
   console.log(data, "data", state);
 
   return (
     <SafeAreaView>
       <View className="flex gap-5 justify-center items-center w-full h-screen">
-        <Text className="text-2xl text-gray-600">
-          Expo starter from yarn monorepo start
-        </Text>
-        <Text className="text-3xl text-gray-400">@ETM Software PLC.</Text>
+        <Text>Expo starter from yarn monorepo start</Text>
+        <Layout level="1">
+          <Text category="c2">
+            {`Selected date: ${date.toLocaleDateString()}`}
+          </Text>
+
+          <Datepicker date={date} onSelect={(nextDate) => setDate(nextDate)} />
+        </Layout>
+        <Text>@ETM Software PLC.</Text>
       </View>
     </SafeAreaView>
   );
