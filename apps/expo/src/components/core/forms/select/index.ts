3e6d9abba9b005833da7bb731534b783e66a55d1
Dawit Assaye <dawit@etmsoftwareplc.com>
2025-01-29 11:40:19 +0300

    fix(App): name fixing on select component

--- apps/expo/src/components/core/forms/select/index.ts
+++ apps/expo/src/components/core/forms/select/index.ts
@@ -1,10 +1,1 @@
-export * from "./Select";
 export * from "./MultiSelect";
-
-export type OptionValue = string | number;
-
-export interface SelectOption {
-  label: string;
-  value: OptionValue;
-  disabled?: boolean;
-}
