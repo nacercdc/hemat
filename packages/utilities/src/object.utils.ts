52d37e792c35f313494223cca21ba4809a0f7f5b
Kabtamu Degifie <kabtamu@etmsoftwareplc.com>
2025-02-03 14:38:20 +0300

    refactor: packages

--- packages/utilities/src/object.utils.ts
+++ packages/utilities/src/object.utils.ts
@@ -1,11 +1,10 @@
-/* eslint-disable @typescript-eslint/no-empty-object-type */
-export function omit<T extends {}, K extends keyof T>(obj: T, ...keys: K[]) {
+export function omit<T extends Record<string, unknown>, K extends keyof T>(obj: T, ...keys: K[]) {
   return Object.fromEntries(
     Object.entries(obj).filter(([key]) => !keys.includes(key as K))
   ) as Omit<T, K>;
 }
 
-export function pick<T extends {}, K extends keyof T>(obj: T, ...keys: K[]) {
+export function pick<T extends Record<string, unknown>, K extends keyof T>(obj: T, ...keys: K[]) {
   return Object.fromEntries(
     keys.filter((key) => key in obj).map((key) => [key, obj[key]])
   ) as Pick<T, K>;
