47b9e89db247ed7d300919c76d36f40d585079f4
Kabtamu Degifie <kabtamu@etmsoftwareplc.com>
2025-02-05 09:22:31 +0300

    chore: update text component

--- packages/rn-ui-components/src/components/presentations/text/Text.tsx
+++ packages/rn-ui-components/src/components/presentations/text/Text.tsx
@@ -1,6 +1,7 @@
+import type { ComponentProps } from 'react';
 import React from 'react'
 import {Text as NWText} from "../../../nativewindui/components/text/Text"
-type Props= React.ComponentProps<typeof NWText>
+type Props= ComponentProps<typeof NWText>
 
 export const Text = (props: Props) => {
   return <NWText {...props} />
