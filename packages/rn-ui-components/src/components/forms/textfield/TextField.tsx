faccbf190fd17eea15c889d75ec0d0c2f206c40b
Dawit Assaye <dawit@etmsoftwareplc.com>
2025-02-06 10:47:57 +0300

    fix(rn-ui-components): textfield customized

--- packages/rn-ui-components/src/components/forms/textfield/TextField.tsx
+++ packages/rn-ui-components/src/components/forms/textfield/TextField.tsx
@@ -1,25 +1,25 @@
 import type { ComponentPropsWithoutRef } from 'react';
 import React from 'react';
 import { TextField as NWTextField } from '../../../nativewindui/components/textfield';
-import type { VariantProps } from 'class-variance-authority';
-import { cva  } from 'class-variance-authority';
-import { cn } from '../../../nativewindui/lib/cn.util';
+import {omit} from "@e-market/utilities"
+import { View } from '../../presentations/view/View';
+import { Text } from '../../presentations/text/Text';
 
-const textfieldVariants = cva(['border border-gray-300 rounded-md'], {
-  variants: {
-    size: {
-      sm: 'h-9 px-2 text-sm',
-      md: 'h-11 px-3 text-base',
-      lg: 'h-13 px-4 text-lg',
-    },
-  },
-  defaultVariants: {
-    size: 'md',
-  },
-});
 
-type Props = ComponentPropsWithoutRef<typeof NWTextField> & VariantProps<typeof textfieldVariants>;
+type Props = Omit<ComponentPropsWithoutRef<typeof NWTextField>,"className"|"style"|"labelClassName"|"containerClassName"|"placeholderClassName" > 
 
-export const TextField = ({ size, className, ...rest }: Props) => {
-  return <NWTextField {...rest} className={cn(textfieldVariants({ size }), className)} />;
-};
+export const TextField = ({...props }: Props) => {
+return(
+    <View>
+    <NWTextField 
+      {...omit(props as ComponentPropsWithoutRef<typeof NWTextField>, "className", "style", "labelClassName", "containerClassName", "placeholderClassName")}
+    />
+     {props.errorMessage && (
+       <Text className="text-destructive mt-1">
+         {props.errorMessage}
+       </Text>
+     )}
+   </View>
+) 
+  };
+    
