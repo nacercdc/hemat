de1b77cd9bd70169e76a672c7658a5a74e26c2cf
Dawit Assaye <dawit@etmsoftwareplc.com>
2025-02-06 16:40:36 +0300

    fix(rn-ui-components): checkbox with label

---
+++ packages/rn-ui-components/src/components/forms/checkbox/Checkbox.tsx
@@ -0,0 +1,49 @@
+import type { ComponentPropsWithoutRef } from "react";
+import { Checkbox as NWCheckbox } from "../../../nativewindui/components/checkbox/Checkbox";
+import { FormController } from "../helper/FormController";
+import { Text } from "../../../nativewindui/components/text/Text";
+import { TouchableOpacity } from "react-native";
+import { omit } from "@e-market/utilities";
+
+interface Props
+  extends Omit<ComponentPropsWithoutRef<typeof NWCheckbox>, "onCheckedChange"> {
+  caption?: string;
+  errorMessage?: string;
+  label?: string;
+  onChange?: (checked: boolean) => void;
+}
+
+export const Checkbox = ({
+  caption,
+  errorMessage,
+  label,
+  checked = false,
+  onChange,
+  ...props
+}: Props) => {
+  const handlePress = () => {
+    onChange?.(!checked);
+  };
+
+  return (
+    <FormController errorMessage={errorMessage} caption={caption}>
+      <TouchableOpacity
+        onPress={handlePress}
+        activeOpacity={1}
+        className="flex flex-row items-center gap-2"
+      >
+        <NWCheckbox
+          checked={checked}
+          onCheckedChange={handlePress}
+          {...omit(
+            props as ComponentPropsWithoutRef<typeof NWCheckbox>,
+            "onCheckedChange"
+          )}
+        />
+        {label && (
+          <Text className="text-sm text-primary-foreground">{label}</Text>
+        )}
+      </TouchableOpacity>
+    </FormController>
+  );
+};
