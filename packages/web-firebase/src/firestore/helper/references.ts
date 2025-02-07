3cb204dded2e8daed87b24eb23f6e99701e1e55a
Kabtamu Degifie <kabtamu@etmsoftwareplc.com>
2025-02-07 11:00:58 +0300

    feat: added reusable references

---
+++ packages/web-firebase/src/firestore/helper/references.ts
@@ -0,0 +1,32 @@
+import type {
+  CollectionReference,
+  DocumentData,
+  DocumentReference,
+  Firestore,
+  QueryConstraint,
+} from "firebase/firestore";
+import { collection, doc, query } from "firebase/firestore";
+import { converter } from "./converter";
+
+export const collectionReference = <T = DocumentData>(
+  firestore: Firestore,
+  collectionName: string
+) => {
+  return collection(firestore, collectionName) as CollectionReference<T>;
+};
+
+export const documentReference = <T = DocumentData>(
+  firestore: Firestore,
+  collectionName: string,
+  id: string
+) => doc(firestore, collectionName, id) as DocumentReference<T>;
+
+export const queryReference = <T extends DocumentData>(
+  firestore: Firestore,
+  collectionName: string,
+  queryConstraint: QueryConstraint[]
+) =>
+  query(
+    collectionReference<T>(firestore, collectionName),
+    ...queryConstraint
+  ).withConverter(converter<T>());
