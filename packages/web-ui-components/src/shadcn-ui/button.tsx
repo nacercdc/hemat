2c0d4015ed45d024e78b52733ddb3a4de3bb6c34
michaelz141 <michael.zerihun@etmsoftwareplc.com>
2025-02-12 19:34:21 +0300

    refactor(button): removed the unnecessary gap and improved dark/basic variant

--- packages/web-ui-components/src/shadcn-ui/button.tsx
+++ packages/web-ui-components/src/shadcn-ui/button.tsx
@@ -6,7 +6,7 @@ import { cva } from "class-variance-authority";
 import { cn } from "./utils/cn";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
+  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
   {
     variants: {
       variant: {
@@ -32,7 +32,7 @@ const buttonVariants = cva(
       variant: "default",
       size: "default",
     },
-  },
+  }
 );
 
 export interface ButtonProps
@@ -51,7 +51,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
         {...props}
       />
     );
-  },
+  }
 );
 Button.displayName = "Button";
 
