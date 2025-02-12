ba9b2eec3d8cdc44a8639307c8b39adcd6f3df9e
Kabtamu Degifie <kabtamu@etmsoftwareplc.com>
2025-02-12 15:57:56 +0300

    fix: remove temp data

--- apps/app/src/components/modules/home/HomeScreen.tsx
+++ apps/app/src/components/modules/home/HomeScreen.tsx
@@ -1,12 +1,8 @@
 import { SafeAreaView } from "react-native";
 import React from "react";
 import { View, Text } from "@e-market/rn-ui-components";
-import useGetLanguages from "~/hooks/rn-firebase/example.hooks";
 
 export default function HomeScreen() {
-  const { data } = useGetLanguages();
-  console.log(data, "data....");
-
   return (
     <SafeAreaView>
       <View className="flex gap-5 px-6 w-full h-screen">
