b3f5ba91b4e481ec528684d1dd5657ee062ed70f
Kabtamu Degifie <kabtamu@etmsoftwareplc.com>
2025-02-17 14:15:52 +0300

    fix: remove unused utils

--- apps/app/src/components/modules/home/HomeScreen.tsx
+++ apps/app/src/components/modules/home/HomeScreen.tsx
@@ -1,11 +1,7 @@
 import React from "react";
 import { View, Text } from "@e-market/rn-ui-components";
-import { useGetLanguages } from "~/hooks/rn-firebase/example.hooks";
 
 export default function HomeScreen() {
-  const { data } = useGetLanguages();
-  console.log(data, "data");
-
   return (
     <View>
       <Text>HomeScreen</Text>
