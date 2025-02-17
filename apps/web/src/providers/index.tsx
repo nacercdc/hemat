e9eb4cc11f8905e9a34046bc0d7aafa1d0ebff11
Kabtamu Degifie <kabtamu@etmsoftwareplc.com>
2025-02-17 10:29:04 +0300

    refactor: web firebase

--- apps/web/src/providers/index.tsx
+++ apps/web/src/providers/index.tsx
@@ -1,17 +1,27 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 "use client";
-
-import { FirebaseProvider } from "@e-market/tanstack-web-firebase-query";
-import { firestore, storage } from "~/configs/firebase.config";
+import {
+  FirebaseProvider,
+  initFirebaseApp,
+} from "@e-market/tanstack-web-firebase-query";
 import TanstackQueryProvider from "./tanstack-query/TanstackQueryProvider";
 import { SidebarProvider } from "@e-market/web-ui-components";
+import { env } from "~/env";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <TanstackQueryProvider>
-      <FirebaseProvider firestore={firestore} storage={storage}>
-        <SidebarProvider>
-           {children}
-         </SidebarProvider>
+      <FirebaseProvider
+        {...initFirebaseApp({
+          apiKey: env.NEXT_PUBLIC_FIREBASE_API_KEY,
+          authDomain: env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+          projectId: env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+          storageBucket: env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+          messagingSenderId: env.NEXT_PUBLIC_FIREBASE_MESSAGE_SENDER_ID,
+          appId: env.NEXT_PUBLIC_FIREBASE_APP_ID,
+        })}
+      >
+        <SidebarProvider>{children}</SidebarProvider>
       </FirebaseProvider>
     </TanstackQueryProvider>
   );
