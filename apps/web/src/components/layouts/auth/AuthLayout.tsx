022504599350f1936da2b0aa7d2abd7af105a4ea
Kabtamu Degifie <habtamu.admas12@gmail.com>
2025-04-28 11:49:30 +0300

    chore: update auth layout

--- apps/web/src/components/layouts/auth/AuthLayout.tsx
+++ apps/web/src/components/layouts/auth/AuthLayout.tsx
@@ -14,8 +14,10 @@ export function AuthLayout({ children }: Props) {
         className="absolute left-24 top-10"
         alt="Vital Work-life"
       />
+      <div className="w-[30rem] h-[30rem] rounded-full absolute top-1/2 left-1/2 translate-x-[-20%] translate-y-[-17%] bg-[radial-gradient(circle,theme(colors.primary.600)_0%,transparent_70%)]"></div>
+
       <div className="w-full min-[500px]:w-[70%] relative sm:max-w-[60%] md:max-w-[45%] lg:max-w-[35%] xl:max-w-[30%] 2xl:max-w-[25%] mx-auto">
-        <div className="pt-4 bg-white pb-8 px-8 rounded-[32px] border border-secondary-50 flex flex-col justify-center max-h-max gap-6 shadow-md shadow-basic-200 relative">
+        <div className="pt-4 bg-white pb-8 px-8 rounded-[32px] border-secondary border-1 border-t-1 flex flex-col justify-center max-h-max gap-6 shadow-sm shadow-basic-400 relative">
           <div className="w-20 h-16 border-[1.5px] border-t-4 border-b-0 border-r-0 top-0 left-[-1px] absolute rounded-tl-[32px] border-l-primary-900 border-t-primary-900 bg-transparent" />
           {children}
         </div>
