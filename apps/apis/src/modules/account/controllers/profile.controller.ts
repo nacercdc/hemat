3d19f6f5165e1bbf89bcf05e66f29e788c8b62ae
wondwosen-bewketu <wondwossen@etmsoftwareplc.com>
2025-05-15 15:02:56 +0300

    feat(apis): add account module controllers

---
+++ apps/apis/src/modules/account/controllers/profile.controller.ts
@@ -0,0 +1,80 @@
+import {
+  Controller,
+  Put,
+  Post,
+  Body,
+  HttpStatus,
+  HttpCode,
+  UseGuards,
+} from '@nestjs/common';
+import {
+  ApiBadRequestResponse,
+  ApiBearerAuth,
+  ApiCreatedResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+  ApiTooManyRequestsResponse,
+  ApiUnauthorizedResponse,
+  ApiUnprocessableEntityResponse,
+} from '@nestjs/swagger';
+import { Throttle } from '@nestjs/throttler';
+import {
+  AuthGuard,
+  AuthDto,
+  AuthUser,
+  Abilities,
+} from '../../../shared/modules';
+import { ExceptionResponseDto } from '../../../shared/dtos';
+import { ProfileService } from '../services';
+import { ProfileCreateRequestDto, AccountResponseDto } from '../dtos';
+
+@ApiBearerAuth()
+@ApiTags('Profiles')
+@ApiBadRequestResponse({
+  description: 'Bad Request',
+  type: ExceptionResponseDto,
+})
+@ApiUnauthorizedResponse({
+  description: 'Unauthorized',
+  type: ExceptionResponseDto,
+})
+@ApiUnprocessableEntityResponse({
+  description: 'Unprocessable Entity',
+  type: ExceptionResponseDto,
+})
+@ApiTooManyRequestsResponse({
+  description: 'Too Many Requests',
+  type: ExceptionResponseDto,
+})
+@UseGuards(AuthGuard)
+@Controller('profiles')
+export class ProfileController {
+  constructor(private readonly profileService: ProfileService) {}
+
+  @ApiOperation({ summary: 'Create', description: 'Create profile.' })
+  @ApiCreatedResponse({ description: 'Created', type: AccountResponseDto })
+  @HttpCode(HttpStatus.CREATED)
+  @Abilities({ isAdmin: false })
+  @Throttle({ default: { limit: 5, ttl: 60000 } })
+  @Post()
+  async create(
+    @AuthUser() auth: AuthDto,
+    @Body() payload: ProfileCreateRequestDto,
+  ) {
+    return this.profileService.updateProfile(auth, payload);
+  }
+
+  @ApiOperation({ summary: 'Update', description: 'Update profile.' })
+  @ApiOkResponse({ description: 'Ok', type: AccountResponseDto })
+  @HttpCode(HttpStatus.OK)
+  @Abilities({ isAdmin: false })
+  @Throttle({ default: { limit: 5, ttl: 60000 } })
+  @Put()
+  async update(
+    @AuthUser() auth: AuthDto,
+    @Body() payload: ProfileCreateRequestDto,
+  ) {
+    return this.profileService.updateProfile(auth, payload);
+  }
+}
---
+++ apps/apis/src/modules/account/controllers/user.controller.ts
@@ -0,0 +1,102 @@
+import {
+  Body,
+  Controller,
+  Get,
+  Ip,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
+import {
+  ApiBadRequestResponse,
+  ApiBearerAuth,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
+import { Throttle } from '@nestjs/throttler';
+import { UserService } from '../services/user.service';
+import {
+  LoginRequestDto,
+  RegisterRequestDto,
+  ChangePasswordRequestDto,
+  AccountResponseDto,
+} from '../dtos';
+import { AuthGuard, AuthRefreshGuard } from '../../../shared';
+import { AuthDto, LoginResponseDto } from '../../../shared/modules';
+import { SuccessResponseDto } from '../../../shared/dtos';
+
+@ApiTags('Auth')
+@Controller('auth')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @ApiOperation({ summary: 'Register a new user' })
+  @ApiOkResponse({ type: SuccessResponseDto })
+  @ApiBadRequestResponse()
+  @Throttle({ default: { limit: 5, ttl: 60000 } })
+  @Post('register')
+  async register(
+    @Body() payload: RegisterRequestDto,
+  ): Promise<SuccessResponseDto> {
+    return this.userService.register(payload);
+  }
+
+  @ApiOperation({ summary: 'Login a user' })
+  @ApiOkResponse({ type: LoginResponseDto })
+  @ApiBadRequestResponse()
+  @Throttle({ default: { limit: 5, ttl: 60000 } })
+  @Post('login')
+  async login(
+    @Body() payload: LoginRequestDto,
+    @Ip() ip: string,
+  ): Promise<LoginResponseDto> {
+    return this.userService.login(payload, ip);
+  }
+
+  @ApiOperation({ summary: 'Logout a user' })
+  @ApiOkResponse({ type: SuccessResponseDto })
+  @ApiBearerAuth()
+  @ApiUnauthorizedResponse()
+  @UseGuards(AuthGuard)
+  @Post('logout')
+  async logout(@Request() req: { user: AuthDto }): Promise<SuccessResponseDto> {
+    return this.userService.logout(req.user);
+  }
+
+  @ApiOperation({ summary: 'Get user information' })
+  @ApiOkResponse({ type: AccountResponseDto })
+  @ApiBearerAuth()
+  @ApiUnauthorizedResponse()
+  @UseGuards(AuthGuard)
+  @Get('me')
+  async me(@Request() req: { user: AuthDto }): Promise<AccountResponseDto> {
+    return this.userService.me(req.user);
+  }
+
+  @ApiOperation({ summary: 'Refresh JWT token' })
+  @ApiOkResponse({ type: LoginResponseDto })
+  @ApiBearerAuth()
+  @ApiUnauthorizedResponse()
+  @UseGuards(AuthRefreshGuard)
+  @Post('refresh-token')
+  async refreshToken(
+    @Request() req: { user: AuthDto },
+  ): Promise<LoginResponseDto> {
+    return this.userService.refreshToken(req.user);
+  }
+
+  @ApiOperation({ summary: 'Change user password' })
+  @ApiOkResponse({ type: SuccessResponseDto })
+  @ApiBearerAuth()
+  @ApiUnauthorizedResponse()
+  @UseGuards(AuthGuard)
+  @Post('change-password')
+  async changePassword(
+    @Request() req: { user: AuthDto },
+    @Body() payload: ChangePasswordRequestDto,
+  ): Promise<SuccessResponseDto> {
+    return this.userService.changePassword(req.user, payload);
+  }
+}
