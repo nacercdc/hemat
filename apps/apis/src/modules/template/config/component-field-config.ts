8927ec0a42689ff61df93b32eebf38b399ebdbeb
Wondwosen Bewketu <wondwosenabebe88@gmail.com>
2025-05-19 10:20:46 +0300

    feat(apis): add component module

---
+++ apps/apis/src/modules/template/config/component-field-config.ts
@@ -0,0 +1,27 @@
+// component-field-config.ts
+export const COMPONENT_FIELD_CONFIG = {
+  includeRelations: ['domain', 'subComponents'],
+  selectableFields: [
+    'id',
+    'code',
+    'name',
+    'description',
+    'domainId',
+    'subComponents.id',
+    'subComponents.code',
+    'subComponents.name',
+  ],
+  searchableFields: ['name', 'description', 'code'],
+  filterableFields: ['domainId'],
+  sortableFields: ['name', 'code'],
+  baseFields: [
+    'id',
+    'code',
+    'name',
+    'description',
+    'domainId',
+    'subComponents.id',
+    'subComponents.code',
+    'subComponents.name',
+  ],
+};
---
+++ apps/apis/src/modules/template/dtos/component-update-request.dto.ts
@@ -0,0 +1,72 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import {
+  IsString,
+  IsNotEmpty,
+  Length,
+  IsUUID,
+  IsOptional,
+} from 'class-validator';
+import { Type } from 'class-transformer';
+
+export class ComponentUpdateRequestDto {
+  @ApiProperty({
+    description: 'ID of the component',
+    example: '123e4567-e89b-12d3-a456-426614174000',
+    type: String,
+  })
+  @IsNotEmpty({ message: 'validation.id.isNotEmpty' })
+  @IsString({ message: 'validation.id.isString' })
+  @Type(() => String)
+  id: string;
+
+  @ApiPropertyOptional({
+    description: 'Unique code of the component',
+    example: '1.A',
+    minLength: 1,
+    maxLength: 50,
+    type: String,
+  })
+  @IsOptional()
+  @IsString({ message: 'validation.code.isString' })
+  @Length(1, 50, { message: 'validation.code.length args: min:1 | max:50' })
+  @Type(() => String)
+  code?: string;
+
+  @ApiPropertyOptional({
+    description: 'Name of the component',
+    example: 'Vaccination Program',
+    minLength: 1,
+    maxLength: 100,
+    type: String,
+  })
+  @IsOptional()
+  @IsString({ message: 'validation.name.isString' })
+  @Length(1, 100, { message: 'validation.name.length args: min:1 | max:100' })
+  @Type(() => String)
+  name?: string;
+
+  @ApiPropertyOptional({
+    description: 'Description of the component',
+    example: 'Component for vaccination initiatives',
+    minLength: 1,
+    maxLength: 500,
+    type: String,
+  })
+  @IsOptional()
+  @IsString({ message: 'validation.description.isString' })
+  @Length(1, 500, {
+    message: 'validation.description.length args: min:1 | max:500',
+  })
+  @Type(() => String)
+  description?: string;
+
+  @ApiPropertyOptional({
+    description: 'ID of the associated domain',
+    example: '123e4567-e89b-12d3-a456-426614174000',
+    type: String,
+  })
+  @IsOptional()
+  @IsUUID('4', { message: 'validation.domainId.isUUID' })
+  @Type(() => String)
+  domainId?: string;
+}
