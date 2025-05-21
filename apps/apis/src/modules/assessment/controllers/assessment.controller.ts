f6f2a433f82838e0035c3f40d748339f58991807
wondwosen-bewketu <wondwossen@etmsoftwareplc.com>
2025-05-21 17:12:17 +0300

    fix: fix the assessment controller

--- apps/apis/src/modules/assessment/controllers/assessment.controller.ts
+++ apps/apis/src/modules/assessment/controllers/assessment.controller.ts
@@ -85,7 +85,8 @@ export class AssessmentController {
   @Get()
   async findAll(@Query() query: QueryManyRequestDto) {
     query.select ??= ASSESSMENT_FIELD_CONFIG.baseFields.join(',');
-    query.include ??= '';
+    query.include ??=
+      'user,country,domains,components,subComponents.measurementScales,subComponents.measurementScales.measurementScale';
     return this.assessmentService.findAll({ query });
   }
 
@@ -107,7 +108,8 @@ export class AssessmentController {
     @Query() query: QueryOneRequestDto,
   ) {
     query.select ??= ASSESSMENT_FIELD_CONFIG.baseFields.join(',');
-    query.include ??= ''; // Avoid fetching relations by default
+    query.include ??=
+      'user,country,domains,components,subComponents.measurementScales,subComponents.measurementScales.measurementScale';
     return this.assessmentService.findOne(id, { query });
   }
 
