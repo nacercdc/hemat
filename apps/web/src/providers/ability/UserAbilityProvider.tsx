f4e5fc5dfa00300e77bba41557a43aa3ffefba1b
Kabtamu Degifie <habtamu.admas12@gmail.com>
2025-06-02 11:58:44 +0300

    feat: ability setup

--- apps/web/src/providers/ability/UserAbilityProvider.tsx
+++ apps/web/src/providers/ability/UserAbilityProvider.tsx
@@ -5,20 +5,30 @@ import React, { useEffect, useState } from "react";
 import type { RawRuleTypes } from "./casl/types";
 import { buildAbilityFor } from "./casl/ability";
 import { AbilityContext } from "./casl/Can";
-import { mapPermissions as _ } from "./casl/utils/split-array.util";
+import { useGetMe } from "../me/useGetMe";
 
 interface Props {
   children: React.ReactNode;
 }
 export default function UserAbilityProvider({ children }: Props) {
-  const [rolePermissions, _setRolePermissions] = useState<RawRuleTypes[]>([]);
-
+  const [rolesPermissions, setRolesPermissions] = useState<RawRuleTypes[]>([]);
+  const { data: currentUser, ...currentUserState } = useGetMe();
   useEffect(() => {
-  // TODO: populate "can" configs
-  }, []);
+    if (currentUserState.isSuccess && currentUser) {
+      const permissions = [
+        ...new Map(
+          currentUser.roles
+            .flatMap((role) => role.permissions)
+            .concat(currentUser.permissions)
+            .map((perm) => [`${perm.action}:${perm.subject}`, perm])
+        ).values(),
+      ];
+      setRolesPermissions(permissions);
+    }
+  }, [currentUserState.isSuccess, currentUser]);
 
   return (
-    <AbilityContext.Provider value={buildAbilityFor(rolePermissions)}>
+    <AbilityContext.Provider value={buildAbilityFor(rolesPermissions)}>
       {children}
     </AbilityContext.Provider>
   );
