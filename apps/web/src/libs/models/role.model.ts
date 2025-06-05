7b4458a6297e6566ebb57451064045b9d0b9cbb3
Joseph Wondwosen <joseph@etmsoftwareplc.com>
2025-06-05 14:16:21 +0300

    chore: updated role, user and permission models

--- apps/web/src/libs/models/role.model.ts
+++ apps/web/src/libs/models/role.model.ts
@@ -1,3 +1,4 @@
+import type { Filter, Sort } from "../tanstack-api-query/helpers/types";
 import type { Permission } from "./permission.model";
 
 export interface Role {
@@ -8,3 +9,16 @@ export interface Role {
   createdAt: string;
   updatedAt: string;
 }
+
+export interface CreateRole {
+  name: string;
+  description?: string;
+  permissionIds: string[];
+}
+
+export type RoleSortable = "name" | "created_at";
+export type RoleFilterable = "is_active" | "created_at";
+export type RoleIncludable = "permissions";
+
+export type RoleSorts = Sort<RoleSortable>[];
+export type RoleFilters = Filter<RoleFilterable>[];
