ab13e2b48d64d8790037ae950acf2aaa7f2dad6a
Befkadu <befkadu.beletee@gmail.com>
2025-06-11 16:37:28 +0300

    fix: route Fix on the assessment page

--- apps/web/src/components/modules/assessment/detail/index.tsx
+++ apps/web/src/components/modules/assessment/detail/index.tsx
@@ -1,15 +1,28 @@
-import React from "react";
+"use client";
+
+import React, { useCallback } from "react";
 
 import { CurrentAssessment } from "./components/tabs/current-assessment";
 import { Tabs } from "@etm/web-ui-components";
 import MemberInvitation from "./components/tabs/members";
 import AssessmentOverview from "./components/tabs/overview";
 import { PageContainer } from "../../components/PageContainer";
+import { useParams, useRouter } from "next/navigation";
 
 export function AssessmentDetail() {
   const assessmentName = "Assessment 1";
+  const router = useRouter();
+  const params = useParams();
+  const assessmentId = params.id;
+  const onBackHandler = useCallback(() => {
+    router.back();
+  }, [router, assessmentId]);
   return (
-    <PageContainer pageTitle={`${assessmentName}`} includeBreadcrumb={false}>
+    <PageContainer
+      pageTitle={`${assessmentName}`}
+      includeBreadcrumb={false}
+      onBack={onBackHandler}
+    >
       <div className="px-8">
         <Tabs
           options={[
