1dd2f8092783898db21647f26557679e4c8fd09f
Joseph Wondwosen <joseph@etmsoftwareplc.com>
2025-06-19 09:57:19 +0300

    feat: implement role delete

--- apps/web/src/components/modules/administration/components/tabs/roles-tab/table/RolesAction.tsx
+++ apps/web/src/components/modules/administration/components/tabs/roles-tab/table/RolesAction.tsx
@@ -4,13 +4,15 @@ import React, { useCallback, useRef } from "react";
 import { Dialog, DropdownMenu, Modal, useToast } from "@etm/web-ui-components";
 import { Icon } from "@iconify/react/dist/iconify.js";
 import { useQueryClient } from "@tanstack/react-query";
-import { PermissionModule, RoleForm } from "../form";
-import { Permission } from "~/libs/models/permission.model";
+import type { PermissionModule } from "../form";
+import { RoleForm } from "../form";
+import type { Permission } from "~/libs/models/permission.model";
 import { getPermissionIds } from "~/components/modules/administration/utils";
 import type { DialogRef, ModalRef } from "@etm/web-ui-components";
 import type { PermissionType } from "~/components/modules/administration/types";
 import type { Role, UpdateRole } from "~/libs/models/role.model";
 import { usePutMutation } from "~/libs/tanstack-api-query/hooks/usePutMutation";
+import { useDeleteMutation } from "~/libs/tanstack-api-query/hooks/useDeleteMutation";
 
 interface Props {
   role: Role;
@@ -27,13 +29,35 @@ export default function RolesAction({
 }: Props) {
   const updateRoleModalRef = useRef<ModalRef>(null);
   const deleteRoleDialogRef = useRef<DialogRef>(null);
-  const { toast } = useToast();
   const queryClient = useQueryClient();
+  const { toast } = useToast();
 
   const { mutate: updateRole, ...updateRoleState } = usePutMutation<
     Role,
     UpdateRole
   >("roles");
+
+  const { mutate: deleteRole, ...deleteRoleState } =
+    useDeleteMutation<Role>("roles");
+
+  const onDeleteRoleHandler = () => {
+    deleteRole(
+      {
+        id: role.id,
+      },
+      {
+        onSuccess: () => {
+          toast({
+            title: "Success",
+            message: "Role has been deleted successfully!",
+            variant: "success",
+          });
+          queryClient.invalidateQueries({ queryKey: ["/roles"] });
+          deleteRoleDialogRef.current?.closeDialog();
+        },
+      }
+    );
+  };
 
   const onUpdateRoleFormSubmitHandler = (
     roleName: string,
@@ -132,7 +156,7 @@ export default function RolesAction({
           },
         ]}
       />
-      <Modal ref={updateRoleModalRef} title="Edit User">
+      <Modal ref={updateRoleModalRef} title="Edit Role">
         <RoleForm
           onSubmitRoleFormHandler={onUpdateRoleFormSubmitHandler}
           rolePermissions={rolePermissions()}
@@ -146,10 +170,10 @@ export default function RolesAction({
       <Dialog
         ref={deleteRoleDialogRef}
         actionLabel="Yes"
-        onAction={() => {
-          //TODO: implement on delete role action
-        }}
+        onAction={onDeleteRoleHandler}
+        actionLoading={deleteRoleState.isPending}
         title="Delete Role"
+        autoClosable={false}
       >
         Are you sure you want to delete this role?
       </Dialog>
