62ca1f4f39d95eb1ca6258a7ba7f87bad4cd2a0d
Joseph Wondwosen <joseph@etmsoftwareplc.com>
2025-06-26 17:52:27 +0300

    chore: add typing and click handler to tabs

--- packages/web-ui-components/src/layouts/tabs/Tabs.tsx
+++ packages/web-ui-components/src/layouts/tabs/Tabs.tsx
@@ -5,18 +5,23 @@ import {
   TabsTrigger,
 } from "../../shadcn-ui";
 
-interface TabOption {
-  value: string;
+interface TabOption<T extends string> {
+  value: T;
   label: React.ReactNode;
   content: React.ReactNode;
 }
 
-interface Props {
-  defaultValue: string;
-  options: TabOption[];
+interface Props<T extends string> {
+  defaultValue: T;
+  options: TabOption<T>[];
+  onTabClick?: (value: T) => void;
 }
 
-export function Tabs({ defaultValue, options }: Props) {
+export function Tabs<T extends string>({
+  defaultValue,
+  options,
+  onTabClick,
+}: Props<T>) {
   return (
     <ShadcnTabs defaultValue={defaultValue} className="px-0">
       <TabsList className="flex w-full gap-6 !bg-transparent justify-start mb-6">
@@ -24,6 +29,7 @@ export function Tabs({ defaultValue, options }: Props) {
           <TabsTrigger
             key={op.value}
             value={op.value}
+            onClick={() => onTabClick?.(op.value)}
             className="data-[state=active]:shadow-none data-[state=active]:border-b-[0.2rem] data-[state=active]:text-primary data-[state=active]:font-bold !border-primary border-solid !bg-transparent !ring-offset-transparent rounded-none justify-start text-basic px-0"
           >
             {op.label}
