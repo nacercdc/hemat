a21cafd302444bc796493d44a1bf56d7c5169991
Joseph Wondwosen <joseph@etmsoftwareplc.com>
2025-06-26 15:27:52 +0300

    chore: added spinner to tool-tip for lazy loaded data

--- packages/web-ui-components/src/popups/tooltip/tooltip.tsx
+++ packages/web-ui-components/src/popups/tooltip/tooltip.tsx
@@ -4,18 +4,41 @@ import {
   TooltipContent,
   TooltipTrigger,
 } from "../../shadcn-ui/tooltip";
+import { Spinner } from "../../presentation";
+import { cn } from "../../shadcn-ui/utils/cn";
 
 interface Props {
+  color?: "dark" | "light";
   trigger: React.ReactNode;
-  content: React.ReactNode;
+  content: React.ReactNode | null;
+  contentLoading: boolean;
 }
 
-export function Tooltip({ trigger, content }: Props) {
+export function Tooltip({
+  color = "light",
+  trigger,
+  content,
+  contentLoading,
+}: Props) {
   return (
     <ShadcnTooltip>
       <TooltipTrigger asChild>{trigger}</TooltipTrigger>
-      <TooltipContent className="bg-white text-dark border-dark-lighter/20 border-[1px]">
-        {content}
+      <TooltipContent
+        className={cn(
+          "text-dark border-dark-lighter/20 border-[1px]",
+          color === "dark" && "bg-dark/60 text-white",
+          color === "light" && "bg-white text-dark"
+        )}
+      >
+        {!contentLoading && <div>{content}</div>}
+        {contentLoading && (
+          <div>
+            <Spinner
+              color={color === "light" ? "primary" : "light"}
+              size="sm"
+            />
+          </div>
+        )}
       </TooltipContent>
     </ShadcnTooltip>
   );
