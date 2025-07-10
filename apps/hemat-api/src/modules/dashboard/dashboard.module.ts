ffbfc5537d143f5e4aae6a3f9c2daffc5cd0d6c7
Wondwosen Abebe <wondwosenabebe88@gmail.com>
2025-07-10 14:36:47 +0300

    Feat[hemat-api]: add the dashboard module

---
+++ apps/hemat-api/src/modules/dashboard/dashboard.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { DashboardController } from './controllers/dashboard.controller';
+import { DashboardService } from './services/dashboard.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Country } from '../../database/entities/country.entity';
+import { Domain } from '../../database/entities/domain.entity';
+import { Assessment } from '../../database/entities/assessment.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Country, Domain, Assessment])],
+  controllers: [DashboardController],
+  providers: [DashboardService],
+  exports: [DashboardService],
+})
+export class DashboardModule {} 
