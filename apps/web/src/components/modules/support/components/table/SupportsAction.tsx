b58030bf7da564b50deec4561059396f7fbd578b
Joseph Wondwosen <joseph@etmsoftwareplc.com>
2025-07-14 12:18:13 +0300

    feat: implement support listing

---
+++ apps/web/src/components/modules/support/components/table/SupportsAction.tsx
@@ -0,0 +1,82 @@
+"use client";
+
+import React, { useRef } from "react";
+import { Icon } from "@iconify/react";
+import { Dialog, DropdownMenu, useToast } from "@etm/web-ui-components";
+import { useQueryClient } from "@tanstack/react-query";
+import { useDeleteMutation } from "~/libs/tanstack-api-query/hooks/useDeleteMutation";
+import type { DialogRef } from "@etm/web-ui-components";
+import type { Support } from "~/libs/models/support.model";
+
+interface Props {
+  support: Support;
+  refetch?: () => void;
+}
+export default function SupportAction({ support }: Props) {
+  const deleteSupportDialogRef = useRef<DialogRef>(null);
+
+  const queryClient = useQueryClient();
+
+  const { toast } = useToast();
+
+  const { mutate: deleteSupport, ...deleteSupportState } = useDeleteMutation(
+    `supports/${support.id}`
+  );
+
+  const onDeleteSupportHandler = () => {
+    deleteSupport(
+      {},
+      {
+        onSuccess: () => {
+          toast({
+            title: "Success",
+            message: "Support has been deleted successfully!",
+            variant: "success",
+          });
+          queryClient.invalidateQueries({ queryKey: ["/supports"] });
+          deleteSupportDialogRef.current?.closeDialog();
+        },
+      }
+    );
+  };
+
+  return (
+    <>
+      <DropdownMenu
+        triggerTextAlign="end"
+        align="end"
+        trigger={
+          <Icon
+            icon="mi:options-horizontal"
+            className="text-xl text-right text-dark"
+          />
+        }
+        options={[
+          {
+            value: "delete",
+            label: "Delete",
+            leftNode: (
+              <Icon
+                icon="material-symbols-light:delete-outline"
+                className="!text-xl text-dark"
+              />
+            ),
+            onClick: () => {
+              deleteSupportDialogRef.current?.openDialog();
+            },
+          },
+        ]}
+      />
+      <Dialog
+        ref={deleteSupportDialogRef}
+        actionLabel="Yes"
+        onAction={onDeleteSupportHandler}
+        autoClosable={false}
+        actionLoading={deleteSupportState.isPending}
+        title="Delete Support"
+      >
+        Are you sure you want to delete this support?
+      </Dialog>
+    </>
+  );
+}
---
+++ apps/web/src/components/modules/support/components/table/index.tsx
@@ -0,0 +1,105 @@
+"use client";
+
+import React, { useCallback, useState } from "react";
+import { Icon } from "@iconify/react/dist/iconify.js";
+import { Table as ETMTable } from "@etm/web-ui-components";
+import { DEFAULT_PAGE_INDEX, DEFAULT_PAGE_SIZE } from "~/constants";
+import { EmptyTableDataElement } from "~/components/modules/components/EmptyTableDataElement";
+
+import Toolbar from "./Toolbar";
+import { useFindAll } from "~/libs/tanstack-api-query/hooks/useFindAll";
+import { SupportsTableColumns } from "./SupportsTableColumns";
+import type { SortingState, PaginationState } from "@etm/web-ui-components";
+import type { StatusType } from "./Toolbar";
+import type { QueryManyResponse } from "~/libs/tanstack-api-query/helpers/types";
+import type {
+  Support,
+  SupportIncludable,
+  SupportSortable,
+  SupportSorts,
+} from "~/libs/models/support.model";
+
+export function SupportTable() {
+  const [search, setSearch] = useState("");
+  const [sort, setSort] = useState<SupportSorts>({});
+  const [pagination, setPagination] = useState<PaginationState>({
+    pageIndex: DEFAULT_PAGE_INDEX,
+    pageSize: DEFAULT_PAGE_SIZE,
+  });
+
+  const { data: supports, ...supportsState } = useFindAll<
+    QueryManyResponse<Support>,
+    SupportIncludable,
+    unknown
+  >({
+    path: "/support",
+    queries: {
+      limit: pagination.pageSize,
+      page: pagination.pageIndex + 1,
+      // include: ["issuedBy"],
+      sorts: sort,
+      search,
+    },
+  });
+
+  const onPageChangeHandler = (pageState: PaginationState) => {
+    setPagination(pageState);
+  };
+
+  const onSortingChangeHandler = (sortingState: SortingState) => {
+    const newSort: SupportSorts = {};
+
+    sortingState.forEach((v) => {
+      newSort[v.desc ? "descending" : "ascending"] =
+        `${v.id}` as SupportSortable;
+    });
+
+    setSort(newSort);
+
+    setPagination({
+      pageIndex: DEFAULT_PAGE_INDEX,
+      pageSize: DEFAULT_PAGE_SIZE,
+    });
+  };
+
+  const onSearchFilterChangeHandler = useCallback((value: string) => {
+    setSearch(value);
+
+    setPagination({
+      pageIndex: DEFAULT_PAGE_INDEX,
+      pageSize: DEFAULT_PAGE_SIZE,
+    });
+  }, []);
+
+  const onStatusFilterChangeHandler = useCallback(
+    (_statusType: StatusType[] | undefined) => {
+      //TODO: implement status filter once the backend is ready
+    },
+    []
+  );
+
+  const OnEmptyDataElement = (
+    <EmptyTableDataElement
+      icon={<Icon icon="mdi:support" className="w-16 h-16" />}
+      title="No Supports Found"
+      body="There are no support threads yet."
+    />
+  );
+
+  return (
+    <ETMTable<Support>
+      columns={SupportsTableColumns()}
+      data={(supports?.data as unknown as Support[]) ?? []}
+      totalItems={supports?.total ?? DEFAULT_PAGE_SIZE}
+      onPaginationChange={onPageChangeHandler}
+      isLoading={supportsState.isLoading}
+      onSortingChange={onSortingChangeHandler}
+      onSearchFilterChange={onSearchFilterChangeHandler}
+      pageSizeOptions={[10, 25, 50, 100]}
+      enableRowSelection={false}
+      initialPagination={pagination}
+      onEmptyDataElement={OnEmptyDataElement}
+      toolbar={<Toolbar onStatusTypeCheck={onStatusFilterChangeHandler} />}
+    />
+  );
+}
