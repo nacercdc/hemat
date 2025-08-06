4bde0dd18d85768907ab9dad19aefdfe823b5de1
Joseph Wondwosen <joseph@etmsoftwareplc.com>
2025-08-06 13:01:24 +0300

    feat: make dynamic header and fix fill status @current-assessment

---
+++ apps/web/src/components/modules/assessment/detail/current-assessment/fill/components/PageSubTitle.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import React from "react";
+import { Skeleton } from "@etm/web-ui-components";
+import type { Domain } from "~/libs/models/domain.model";
+import type { QueryManyResponse } from "~/libs/tanstack-api-query/helpers/types";
+import { useFindById } from "~/libs/tanstack-api-query/hooks/useFindById";
+
+interface Props {
+  assessmentId: string;
+  domainId: string;
+}
+export function PageSubTitle({ assessmentId, domainId }: Props) {
+  const { data: domain, ...domainState } = useFindById<
+    QueryManyResponse<Domain>
+  >({
+    path: `/assessments/${assessmentId}/domains/${domainId}`,
+  });
+
+  if (domainState.isLoading || domainState.isFetching) {
+    return <Skeleton className="max-w-80 w-full h-7 rounded-sm" />;
+  }
+  return (
+    <span className="text-xl font-bold">{`${(domain as unknown as Domain)?.name} / Components`}</span>
+  );
+}
