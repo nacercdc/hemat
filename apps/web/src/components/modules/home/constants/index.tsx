8689ce66f28360875a413dea9ea03dc276b2e95f
Joseph Wondwosen <joseph@etmsoftwareplc.com>
2025-08-20 17:15:59 +0300

    chore: added health ministries of countries

--- apps/web/src/components/modules/home/constants/index.tsx
+++ apps/web/src/components/modules/home/constants/index.tsx
@@ -83,6 +83,66 @@ export const PartnersImages = [
     height: 50,
     alt: "world bank logo",
   },
+  {
+    src: "/images/partners/zambia.svg",
+    width: 100,
+    height: 50,
+    alt: "zambia",
+  },
+  {
+    src: "/images/partners/tunisia.svg",
+    width: 160,
+    height: 160,
+    alt: "tunisia",
+  },
+  {
+    src: "/images/partners/republique_togo.svg",
+    width: 100,
+    height: 80,
+    alt: "republique togo",
+  },
+  {
+    src: "/images/partners/republic_of_seychelles.svg",
+    width: 180,
+    height: 180,
+    alt: "republic of seychelles",
+  },
+  {
+    src: "/images/partners/namibia.svg",
+    width: 180,
+    height: 180,
+    alt: "namibia",
+  },
+  {
+    src: "/images/partners/liberia.png",
+    width: 100,
+    height: 60,
+    alt: "liberia",
+  },
+  {
+    src: "/images/partners/kenya.png",
+    width: 100,
+    height: 100,
+    alt: "kenya",
+  },
+  {
+    src: "/images/partners/ivory_coast.svg",
+    width: 120,
+    height: 120,
+    alt: "ivory coast",
+  },
+  {
+    src: "/images/partners/ethiopia.svg",
+    width: 100,
+    height: 60,
+    alt: "ethiopia",
+  },
+  {
+    src: "/images/partners/cameron.svg",
+    width: 100,
+    height: 50,
+    alt: "cameron",
+  },
 ];
 
 export const DomainIconMap: Record<string, string[]> = {
